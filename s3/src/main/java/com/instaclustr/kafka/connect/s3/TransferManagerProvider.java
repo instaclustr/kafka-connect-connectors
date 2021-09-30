9302b3f12c6edb5a9cc7408b80e59dd188227e1f
amrutha_shanbhag <amrutha.shanbhag@instaclustr.com>
2021-09-30 10:39:31 +1000

    implement IAM role support for S3 connector

--- s3/src/main/java/com/instaclustr/kafka/connect/s3/TransferManagerProvider.java
+++ s3/src/main/java/com/instaclustr/kafka/connect/s3/TransferManagerProvider.java
@@ -3,14 +3,20 @@ package com.instaclustr.kafka.connect.s3;
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.STSAssumeRoleSessionCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class TransferManagerProvider {
     private TransferManager transferManager;
@@ -30,9 +36,34 @@ public class TransferManagerProvider {
         String accessKey = getFromConfigOrEnvironment(config, AwsStorageConnectorCommonConfig.AWS_ACCESS_KEY_ID);
         String secret = getFromConfigOrEnvironment(config, AwsStorageConnectorCommonConfig.AWS_SECRET_KEY);
         String region = getFromConfigOrEnvironment(config, AwsStorageConnectorCommonConfig.AWS_REGION);
+        String roleArn = getFromConfigOrEnvironment(config, AwsStorageConnectorCommonConfig.AWS_IAM_ROLE_ARN);
 
-        AmazonS3ClientBuilder clientBuilder = AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secret)));
+        AmazonS3ClientBuilder clientBuilder;
+
+        if (roleArn == null || StringUtils.isBlank(roleArn)) {
+            // authenticate with access key/secret
+            AWSStaticCredentialsProvider awsStaticCredentialsProvider = new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secret));
+            clientBuilder = AmazonS3ClientBuilder.standard()
+                    .withCredentials(awsStaticCredentialsProvider);
+        } else {
+            // authenticate with access key/secret, then assume role
+            AWSSecurityTokenService awsSecurityTokenService = AWSSecurityTokenServiceClientBuilder.standard()
+                    .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secret)))
+                    .build();
+
+            STSAssumeRoleSessionCredentialsProvider.Builder assumeRoleBuilder =
+                    new STSAssumeRoleSessionCredentialsProvider.Builder(
+                            roleArn,
+                            UUID.randomUUID().toString().substring(0, 32));
+
+            STSAssumeRoleSessionCredentialsProvider credentialsProvider = assumeRoleBuilder
+                    .withStsClient(awsSecurityTokenService)
+                    .withRoleSessionDurationSeconds((int) TimeUnit.HOURS.toSeconds(1))
+                    .build();
+
+            clientBuilder = AmazonS3ClientBuilder.standard()
+                    .withCredentials(credentialsProvider);
+        }
 
         if (region == null) {
             region = AwsStorageConnectorCommonConfig.DEFAULT_AWS_REGION;
