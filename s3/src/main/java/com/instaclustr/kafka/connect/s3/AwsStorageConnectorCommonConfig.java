978ed928cc9f0c03aac500c40606ec98bf9e30cf
Niluka Weerawarnakula <niluka@netapp.com>
2025-03-05 17:16:58 +1100

    Bugfix typo

--- s3/src/main/java/com/instaclustr/kafka/connect/s3/AwsStorageConnectorCommonConfig.java
+++ s3/src/main/java/com/instaclustr/kafka/connect/s3/AwsStorageConnectorCommonConfig.java
@@ -39,7 +39,6 @@ public class AwsStorageConnectorCommonConfig {
                 .define(AWS_SECRET_KEY, ConfigDef.Type.PASSWORD, ConfigDef.Importance.HIGH, "AWS access secret key")
                 .define(AWS_REGION, ConfigDef.Type.STRING, DEFAULT_AWS_REGION, ConfigDef.Importance.MEDIUM, String.format("AWS client region, if not set will use %s", DEFAULT_AWS_REGION))
                 .define(AWS_IAM_ROLE_ARN, ConfigDef.Type.STRING, "", ConfigDef.Importance.HIGH, "")
-                .define(S3_ENDPOINT, ConfigDef.Type.STRING, "", ConfigDef.Importance.HIGH, "")
                 .define(S3_ENDPOINT, ConfigDef.Type.STRING, ConfigDef.Importance.MEDIUM, "Optional S3 endpoint URL used to configure non-aws S3 providers");
         return configDef;
     }
