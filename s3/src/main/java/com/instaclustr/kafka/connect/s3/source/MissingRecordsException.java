fea3c7895275bb83c6c3e420d6256e1f8cba4ba2
niluka-insta <93362011+niluka-insta@users.noreply.github.com>
2026-03-17 13:10:23 +1100

    INS-40277 Remove old S3 source connector (#22)
    * INS-40277 deprecate old source
    
    * Updated the version number

--- s3/src/main/java/com/instaclustr/kafka/connect/s3/source/MissingRecordsException.java
+++
@@ -1,7 +0,0 @@
-package com.instaclustr.kafka.connect.s3.source;
-
-public class MissingRecordsException extends RuntimeException {
-    public MissingRecordsException(final String message) {
-        super(message);
-    }
-}
